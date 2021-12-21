b465bd5cffc60353f8c6266efb97cef312f30691
satoshi-361 <reader2027@gmail.com>
2021-12-20 17:27:57 -0800

    lp complete

---
+++ pages/lp2.vue
@@ -0,0 +1,272 @@
+<template>
+  <div class="l-main">
+    <div class="hero-section-merit">
+      <div class="inner">
+        <div class="hero-message-merit">
+          <img class="img" src="../assets/img/cstm_lp/pngwing.com.png" alt="">
+          <h1 class="heading">テックタッチを使うと、<br>誰でも簡単にシステムの利用が可能に<br>デジタルトランスフォーメーションを実現</h1>
+        </div>
+      </div>
+    </div>
+    <div class="content-section">
+      <div class="inner">
+        <ul class="merit-list -second">
+          <li class="item">
+            <div class="content">
+              <div class="number">1</div>
+              <h2 class="title">正しいシステム活用</h2>
+              <p class="description">ナビゲーションを通じて意図通りのシステム利用を促進でき、ユーザーがシステムを使いこなすため、結果としてチャーンレートの抑止につながります。</p>
+            </div>
+            <div class="imgBox -right">
+              <img class="img -merit10" src="../assets/img/cstm_lp/Web1920-1_1.png" alt="正しいシステム活用">
+            </div>
+          </li>
+          <li class="item -rtl">
+            <div class="content">
+              <div class="number">2</div>
+              <h2 class="title">オンボーディング効率向上</h2>
+              <p class="description">ユーザーがナビゲーションを参照してシステム運用を始められるため、オンボーディング工数が削減できます。また、オンボーディング後の問い合わせ数の削減にも繋がります。</p>
+            </div>
+            <div class="imgBox -left">
+              <img class="img -merit10" src="../assets/img/cstm_lp/Web1920-1_2.png" alt="オンボーディング効率向上">
+            </div>
+          </li>
+          <li class="item">
+            <div class="content">
+              <div class="number">3</div>
+              <h2 class="title">ユーザーの声をシステム<br>に即時反映</h2>
+              <p class="description">プログラミング不要でナビゲーションを作成・修正できるため、ユーザーの声をもとにシステムへ即時反映することができます。</p>
+            </div>
+            <div class="imgBox -right">
+              <img class="img -merit10" src="../assets/img/cstm_lp/Web1920-1_3.png" alt="ユーザーの声をシステムに即時反映">
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="content-section -light-blue">
+      <div class="inner">
+        <div class="content-header">
+          <p class="title">デジタルガイド</p>
+          <p class="description">あらゆるシステム上に、ステップ・バイ・ステップの操作ガイドや、入力ルールを示すツールチップを展開することで、ユーザー体験を向上させます。</p>
+          <div class="imgBox -center">
+            <img src="../assets/img/cstm_lp/Rectangle 6.png">
+          </div>
+        </div>
+        <ul class="cstm-list">
+          <li class="item">
+            <p class="title">多様な操作ガイドで、ゴールまでナビゲーション</p>
+            <p class="description">吹き出し、ポップアップ、選択分岐などの多様なガイドステップによって、ユーザーは迷うことなくプロセスを遂行できます。</p>
+            <div class="imgBox -center">
+              <img src="../assets/img/cstm_lp/fore-1.png">
+            </div>
+          </li>
+          <li class="item">
+            <p class="title">画面上の任意の場所に、ヒントや説明を設置</p>
+            <p class="description">用語説明、入力ルールなどのちょっとしたヒントや説明を、画面上の任意の箇所に、ツールチップとして設置することができます。</p>
+            <div class="imgBox -center">
+              <img src="../assets/img/cstm_lp/fore-2.png">
+            </div>
+          </li>
+          <li class="item">
+            <p class="title">条件に応じたガイドの出し分け</p>
+            <p class="description">日時や期間、ブラウザ、デバイスなど、ユーザーの環境に応じて実行される操作ガイドを出し分けることができます。ユーザー毎に最適なガイドを、最適なタイミングで提供可能です。</p>
+            <div class="imgBox -center">
+              <img src="../assets/img/cstm_lp/fore-1.png">
+            </div>
+          </li>
+          <li class="item">
+            <p class="title">誤った入力を未然に防止</p>
+            <p class="description">テキスト入力された内容をリアルタイムに検証し、規則に沿わない内容の入力や入力ミスを未然に防ぐことができます。</p>
+            <div class="imgBox -center">
+              <img src="../assets/img/cstm_lp/fore-1.png">
+            </div>
+          </li>
+          <li class="item">
+            <p class="title">ノーコード、かんたん操作でコンテンツ作成</p>
+            <p class="description">すべてのガイドやツールチップは、利用システム画面上での直感的な操作によって、作成と設置が可能です。作業中のガイドは即座にプレビューでき、ユーザー体験を確認しながら、操作ガイドを作成できます。</p>
+            <div class="imgBox -center">
+              <img src="../assets/img/cstm_lp/fore-1.png">
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="content-section">
+      <div class="inner">
+        <div class="content-header">
+          <p class="title">NPS計測</p>
+          <p class="description">システム利用ユーザーへのNPSアンケート集計によって、システムの顧客ロイヤルティやユーザー満足度を計測できます。</p>
+          <div class="imgBox -center">
+            <img src="../assets/img/cstm_lp/Rectangle 6.png">
+          </div>
+        </div>
+        <ul class="cstm-list">
+          <li class="item -box-shadow">
+            <div class="imgBox -center">
+              <img src="../assets/img/cstm_lp/fore-1.png">
+            </div>
+            <p class="title -center">多様な操作ガイドで、ゴールまでナビゲーション</p>
+            <p class="description">0-10の11段階でのユーザー評価に加え、自由回答の収集により、顧客ロイヤリティ度合いの把握と顧客フィードバックが獲得できます。</p>
+          </li>
+          <li class="item -box-shadow">
+            <div class="imgBox -center">
+              <img src="../assets/img/cstm_lp/fore-1.png">
+            </div>
+            <p class="title -center">ターゲットユーザーへ、任意のタイミングでアンケート表示</p>
+            <p class="description">特定のユーザーやグループ、指定した人数へのランダムサンプリングによるアンケート収集が可能です。また、アンケートはシステム内の任意の場所や特定のユーザー操作後を指定して設置できます。</p>
+          </li>
+          <li class="item -box-shadow">
+            <div class="imgBox -center">
+              <img src="../assets/img/cstm_lp/fore-1.png">
+            </div>
+            <p class="title -center">リアルタイム集計</p>
+            <p class="description">収集されたアンケートはリアルタイムに集計され、NPSスコアや分布、回答率を確認できます。より迅速なユーザーへのアプローチ検討を可能にします。</p>
+          </li>
+        </ul>
+      </div>
+    </div>    
+    <div class="content-section -light-blue">
+      <div class="inner">
+        <div class="content-header">
+          <p class="title">利用分析</p>
+          <p class="description">ユーザーによるシステム利用動向を可視化し、活用されていない機能や、非効率な操作を発見します。</p>
+          <div class="imgBox -center">
+            <img src="../assets/img/cstm_lp/Rectangle 6.png">
+          </div>
+        </div>
+        <ul class="cstm-list">
+          <li class="item -bg-white">
+            <p class="title -center">ユーザーのシステム利用状況を可視化</p>
+            <div class="imgBox -center">
+              <img src="../assets/img/cstm_lp/fore-1.png">
+            </div>
+            <p class="description">ユーザーによるシステム利用頻度や利用時間、利用機能を可視化することで、システム、各機能の活用の実態を把握します。</p>
+           </li>
+          <li class="item -bg-white">
+            <p class="title -center">操作ガイド利用状況を分析</p>
+            <div class="imgBox -center">
+              <img src="../assets/img/cstm_lp/fore-1.png">
+            </div>
+            <p class="description">導入したガイド、ツールチップの利用回数や利用ユーザ数、利用トレンドの分析により、導入時の目標値に対して、常時進捗度合いを確認することができます。</p>
+           </li>
+          <li class="item -bg-white">
+            <p class="title -center">ユーザーの離脱箇所を発見</p>
+            <div class="imgBox -center">
+              <img src="../assets/img/cstm_lp/fore-1.png">
+            </div>
+            <p class="description">ユーザーの行動を分析し、プロセスの途中でユーザーが操作に詰まる箇所や、離脱の多い箇所、離脱理由を探ります。該当箇所に操作ガイドを設置することで、ユーザー体験を向上させます。</p>
+           </li>
+        </ul>
+      </div>
+    </div>
+    <div class="content-section">
+      <div class="inner -wide">
+        <div class="content-header -bg-dark">
+          <p class="title">価格</p>
+          <p class="description -center">お客様に最適な料金プランをご案内いたします 詳しくはお問い合わせください</p>
+          <div class="btnBox">
+            <a class="btn -orange">お問い合わせ</a>
+          </div>
+        </div>
+        <h2 class="title">導入事例 / お客さまの声</h2>
+        <div class="content">
+          <div class="item">
+            <img src="../assets/img/cstm_lp/fore-3.png">
+            <div class="detail">
+              <p class="title">富士通 SAP S/4HANA先行導入に<br>採用されました</p>
+              <p class="label1">富士通株式会社</p>
+              <p class="label2">業種：製造業・メーカー</p>
+              <p class="label2">従業員数：50,000人</p>
+            </div>
+          </div>
+          <div class="item">
+            <img src="../assets/img/cstm_lp/fore-4.png">
+            <div class="detail">
+              <p class="title">富士通 SAP S/4HANA先行導入に<br>採用されました</p>
+              <p class="label1">富士通株式会社</p>
+              <p class="label2">業種：製造業・メーカー</p>
+              <p class="label2">従業員数：50,000人</p>
+            </div>
+          </div>
+          <div class="item">
+            <img src="../assets/img/cstm_lp/fore-5.png">
+            <div class="detail">
+              <p class="title">富士通 SAP S/4HANA先行導入に<br>採用されました</p>
+              <p class="label1">富士通株式会社</p>
+              <p class="label2">業種：製造業・メーカー</p>
+              <p class="label2">従業員数：50,000人</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="content-section">
+      <div class="inner -noPadding">
+        <ul class="logo-list">
+          <li class="item"><img class="logo" src="../assets/img/cstm_lp/link1.png" alt=""></li>
+          <li class="item"><img class="logo" src="../assets/img/cstm_lp/link2.png" alt=""></li>
+          <li class="item"><img class="logo" src="../assets/img/cstm_lp/link3.png" alt=""></li>
+          <li class="item"><img class="logo" src="../assets/img/cstm_lp/link4.png" alt=""></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import axios from 'axios'
+
+@Component({
+  layout: 'cstm_lp'
+})
+export default class Nakanohito extends Vue {
+
+  meta = {
+    title: 'ランディングページ2',
+    description: 'ランディングページ',
+    type: 'lp',
+    url: 'https://techtouch.jp/lp1',
+    image: 'https://techtouch.jp/ogp.png'
+  }
+
+  head() {
+    return {
+      title: this.meta.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.meta.description
+        },
+        { hid: 'og:type', property: 'og:type', content: this.meta.type },
+        { hid: 'og:title', property: 'og:title', content: this.meta.title },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: this.meta.description
+        },
+        { hid: 'og:url', property: 'og:url', content: this.meta.url },
+        { hid: 'og:image', property: 'og:image', content: this.meta.image },
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+@import '../assets/css/iconfonts.css'
+@import '../assets/css/base/mixin.styl'
+@import '../assets/css/base/variables.styl'
+@import '../assets/css/base/reset.styl'
+@import '../assets/css/layout/layout.styl'
+@import '../assets/css/element/btn.styl'
+@import '../assets/css/component/btn-group.styl'
+@import '../assets/css/component/logo-list.styl'
+@import '../assets/css/component/hero-section-merit.styl'
+@import '../assets/css/component/hero-message-merit.styl'
+@import '../assets/css/component/content-section.styl'
+@import '../assets/css/component/merit-list.styl'
+@import '../assets/css/component/cstm.styl'
+</style>
